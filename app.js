de07f85b9489da914281adaf19b62bd1cc74374f
TANG YI <1394855803@qq.com>
2025-12-05 14:46:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const http = require("http");
+// 故意添加未使用的变量（违反eslint的no-unused-vars规则）
+const unusedVariable = "这段代码会触发Lint检查失败";
 // 创建HTTP服务，访问时返回指定内容
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
